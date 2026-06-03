a6717019408a48dada6bba62aba8f5492157a7b5
taejeong1126 <taejeong654@gmail.com>
2026-06-03 20:31:33 +0900

    chore: 프로젝트 초기 설정

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View } from 'react-native';
+
+function App() {
+    return <View style={styles.container} />;
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
+
+export default App;
